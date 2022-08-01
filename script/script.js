667245332765accd7d6f7b262572b10fddb70ee1
Ashish Nagar <ashishnagar033@gmail.com>
2022-08-02 00:05:27 +0530

    first commit

---
+++ script/script.js
@@ -0,0 +1,21 @@
+// hamburger
+
+const hamburgerButton = document.querySelector(".hamburger")
+const navList = document.querySelector("#nav-list")
+const logoNone = document.querySelector(".logo")
+function toggleButton(){
+    navList.classList.toggle('show')
+    logoNone.classList.toggle('logo-none')
+    
+}
+
+hamburgerButton.addEventListener('click', toggleButton)
+
+
+// sticky navbar
+
+window.addEventListener("scroll" , function() {
+     var Nav = document.querySelector("nav");
+     Nav.classList.toggle("sticky", window.scroll)
+     console.log("its not working")
+})
