ebadd465d9bc0ab0c0f940a7cce7ca256990b0f8
ashish nagar <ashishnagar033@gmail.com>
2023-01-12 16:47:27 +0530

    theme color changed in blue

--- script/script.js
+++ script/script.js
@@ -14,7 +14,7 @@ var myScrollFunc = function () {
   var y = window.scrollY;
   let heroHeight = mainHeroAreaHeignt.height - navHeignt.height;
   if (y >= heroHeight) {
-    document.getElementById("nav").style.background = "#242B2E";
+    document.getElementById("nav").style.background = "#101727";
     logoNone.style.display = 'flex'
   } else {
     document.getElementById("nav").style.background = "transparent";
@@ -32,8 +32,6 @@ window.addEventListener("DOMContentLoaded", () => {
     transform: document.querySelector(".particles-js-canvas-el").style.transform =
       "translate(0%, 0%)";
     document.querySelector(".particles-js-canvas-el").style.width = "100%";
-    document.querySelector(".hero-text h1").style.transition =  "background 0.5s ease 3s";
-    document.querySelector(".hero-text h1").style.background = "rgba(36, 43, 46, 0.6)";
   }, 5000);
 })
 
