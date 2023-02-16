2f0d478e9174eb78c22beb07e06bf23410606be7
Ashish Nagar <ashishnagar033@gmail.com>
2023-02-16 16:38:42 +0530

    projects update

--- projects page/projects.js
+++ projects page/projects.js
@@ -13,79 +13,85 @@ let projectDetailsArray = [
     },
     {
       name : 'project 3',
+      url : 'https://fun-with-react-material-ui.netlify.app/',
+      techStack : 'Html,css,js',
+      github: 'https://github.com/Ashish-Nagar-027/React-MUI-Project-'
+    },
+    {
+      name : 'project 4',
       url : 'https://ashish-nagars-html-css-project-15.netlify.app/',
       techStack : 'HtmlAndCss',
       github: 'https://github.com/Ashish-Nagar-027/html-and-css-project-15'
     },
     {
-      name : 'project 4',
+      name : 'project 5',
       url : 'https://shopify-clone-by-ashish.netlify.app/',
       techStack : 'Tailwind',
       github: 'https://github.com/Ashish-Nagar-027/shopify-clone-with-tailwind'
   
     },
     {
-      name : 'project 5',
+      name : 'project 6',
       url : 'https://js-time-player-by-ashish-nagar.netlify.app/',
       techStack : 'javaScript',
       github: 'https://github.com/Ashish-Nagar-027/Js-Time-Player'
     },
     {
-      name : 'project 6',
+      name : 'project 7',
       url : 'https://random-quotes-and-jokes-generator.netlify.app/',
       techStack : 'javaScript',
       github: 'https://github.com/Ashish-Nagar-027/Js-Quote-and-joke-generator'
     },
     {
-      name : 'project 7',
+      name : 'project 8',
       url : 'https://video-player-with-js.netlify.app/',
       techStack : 'javaScript',
       github: 'https://github.com/Ashish-Nagar-027/video-player-with-javaScript'
     },
     {
-      name : 'project 8',
+      name : 'project 9',
       url : 'https://js-word-player-by-ashish-nagar.netlify.app/',
       techStack : 'javaScript',
       github: 'https://github.com/Ashish-Nagar-027/Js-word-player'
     },
     {
-      name : 'project 9',
+      name : 'project 10',
       url : 'https://rode-clone-with-tailwind.netlify.app/',
       techStack : 'Tailwind',
       github: 'https://github.com/Ashish-Nagar-027/Rode.com-clone-with-tailwind'
     },
     {
-      name : 'project 10',
+      name : 'project 11',
       url : 'https://ashish-nagars-html-css-project-12.netlify.app/',
       techStack : 'javaScript',
       github: 'https://github.com/Ashish-Nagar-027/html-and-css-project-12'
     },
     {
-      name : 'project 11',
+      name : 'project 12',
       url : 'https://ashish-nagars-html-css-project-11.netlify.app/',
       techStack : 'javaScript',
       github: 'https://github.com/Ashish-Nagar-027/html-and-css-project-11'
     },
     {
-      name : 'project 12',
+      name : 'project 13',
       url : 'https://todo-list-with-js-by-ashish-nagar.netlify.app/',
       techStack : 'javaScript',
       github: 'https://github.com/Ashish-Nagar-027/JS-Todo-List-Project'
     },
     {
-      name : 'project 13',
+      name : 'project 14',
       url : 'https://url-bookmarker-by-ashish-nagar.netlify.app/',
       techStack : 'javaScript',
       github: 'https://github.com/Ashish-Nagar-027/Url-Book-Marker'
     },
     {
-      name : 'project 14',
+      name : 'project 15',
       url : 'https://ashish-nagars-html-css-project-9.netlify.app/',
       techStack : 'javaScript',
       github: 'https://github.com/Ashish-Nagar-027/HTML-and-CSS-project-9'
     },
     {
-      name : 'project 15',
+      name : 'project 16',
       url : 'https://ashish-nagars-html-css-project-1.netlify.app/',
       techStack : 'javaScript',
       github: 'https://github.com/Ashish-Nagar-027/Html-and-CSS-Project-1 '
